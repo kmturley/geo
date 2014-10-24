46bcb1c9f35995d9eeb9dd5c5599c0171e11f2cf
Kim T <kim@bynd.com>
2014-10-24 18:27:53 -0400

    Geo II updated to support lines

--- js/modules/App.js
+++ js/modules/App.js
@@ -21,16 +21,23 @@ var App = function () {
             
             this.addEvent('click', this.inputs, function (e) {
                 if (e.target.checked === true) {
-                    me.geo.add(e.target.value, function (kml) {
-                        var lines = me.geo.calculate(kml);
-                        var html = me.update(lines);
+                    me.geo.load(e.target.value, function (kml) {
                         me.kml = kml;
-                        me.el.innerHTML = html;
-                        
+                        me.update();
                     });
                 } else if (e.target.checked === false) {
                     me.geo.remove(e.target.value);
                 }
+            });
+            
+            this.addEvent('change', document.getElementById('minDeviation'), function (e) {
+                me.geo.options.minDeviation = e.target.value;
+                document.getElementById('minDeviationVal').innerHTML = e.target.value;
+            });
+
+            this.addEvent('change', document.getElementById('maxDeviation'), function (e) {
+                me.geo.options.maxDeviation = e.target.value;
+                document.getElementById('maxDeviationVal').innerHTML = e.target.value;
             });
             
             this.addEvent('change', document.getElementById('minDistance'), function (e) {
@@ -53,17 +60,38 @@ var App = function () {
                 document.getElementById('maxBearingVal').innerHTML = e.target.value;
             });
             
-            this.el.innerHTML = this.update([]);
+            this.addEvent('change', document.getElementById('sliders'), function (e) {
+                me.update();
+            });
+            
+            this.el.innerHTML = this.createFooter([]);
+        },
+        update: function () {
+            this.geo.reset();
+            this.geo.add(this.kml);
+            var lines = this.geo.calculate(this.kml);
+            this.el.innerHTML = this.createFooter(lines);
         },
         /**
-         * @method update
+         * @method createFooter
          */
-        update: function (lines) {
-            var html = '',
+        createFooter: function (lines) {
+            var me = this,
+                i = 0,
+                html = '',
                 show = false;
 
             html += '<div class="col w1of3 h1of1 scroll' + (show ? ' line-hide' : ' line-show') + '"><table><tr><th>Name &#x25BE;</th><th>Distance</th><th>Bearing</th></tr>';
-            html += this.createTable(lines.sort(function (a, b) { return (a.name < b.name) ? -1 : 1; }));
+            html += this.createTable(lines.sort(function (a, b) {
+                // limit lines on google earth to prevent performance issues
+                if (i < 200) {
+                    me.geo.addLine(a);
+                } else if (i === 200) {
+                    alert('There are too many lines to be drawn on the map, try narrowing the line filters');
+                }
+                i += 1;
+                return (a.name < b.name) ? -1 : 1;
+            }));
             html += '</table></div>';
             html += '<div class="col w1of3 h1of1 scroll' + (show ? ' line-hide' : ' line-show') + '"><table><tr><th>Name</th><th>Distance &#x25BE;</th><th>Bearing</th></tr>';
             html += this.createTable(lines.sort(function (a, b) { return a.distance - b.distance; }));
@@ -81,7 +109,7 @@ var App = function () {
             var i = 0,
                 html = '';
             for (i = 0; i < items.length; i += 1) {
-                html += '<tr class="' + (items[i].selected ? 'line-on' : 'line') + '"><td>' + items[i].name + '</td><td>' + items[i].distance.toFixed(2) + 'km</td><td>' + items[i].bearing.toFixed(2) + '°</td></tr>';
+                html += '<tr class="' + (items[i].selected ? 'line-on' : 'line') + '"><td>' + items[i].name + '</td><td>' + items[i].distance.toFixed(2) + 'km</td><td>' + items[i].bearing.toFixed(2) + '° / ' + items[i].bearing2.toFixed(2) + '°</td></tr>';
             }
             if (html === '') {
                 html = '<tr><td colspan="3" style="text-align:center;">No results found</td></tr>';
